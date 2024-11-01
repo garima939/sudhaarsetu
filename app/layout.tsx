63b54fe017e06887ad8c0ac89e38648fbef9b7dd
Anupam Maurya <anupammaurya6767@gmail.com>
2024-11-01 23:53:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './app/page.tsx'
+import Home from './page'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
